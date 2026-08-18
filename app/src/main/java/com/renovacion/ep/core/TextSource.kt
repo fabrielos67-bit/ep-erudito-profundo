a4ab5ec2977062a37fcf5fe52c9276db816d9f51
fabrielos67-bit <fabrielos67@gmail.com>
2026-08-18 16:05:42 -0600

    Create TextSource.kt

---
+++ app/src/main/java/com/renovacion/ep/core/TextSource.kt
@@ -0,0 +1,30 @@
+package com.renovacion.ep.core
+
+enum class CategoriaFuente {
+    TEXTO_HEBREO,
+    TEXTO_GRIEGO,
+    TEXTO_ARAMEO_SIRIACO,
+    TEXTO_INGLES_HISTORICO,
+    TEXTO_ESPANOL,
+    LEXICO,
+    FUENTE_EXTERNA
+}
+
+data class VerseResult(
+    val fuenteId: String,
+    val fuenteNombre: String,
+    val referencia: String,
+    val texto: String?,
+    val disponible: Boolean,
+    val nota: String? = null
+)
+
+interface TextSource {
+    val id: String
+    val nombre: String
+    val idioma: String
+    val periodo: String
+    val categoria: CategoriaFuente
+
+    fun buscar(referencia: Reference): VerseResult
+}
