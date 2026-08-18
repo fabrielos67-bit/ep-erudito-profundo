c1ac11a5f1dffba5fd6cf91e1b93822fde016426
fabrielos67-bit <fabrielos67@gmail.com>
2026-08-18 16:11:29 -0600

    Create Color.kt

---
+++ app/src/main/java/com/renovacion/ep/ui/theme/Color.kt
@@ -0,0 +1,12 @@
+package com.renovacion.ep.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val FondoOscuro = Color(0xFF0B0B0B)
+val SuperficieOscura = Color(0xFF171512)
+val SuperficieOscuraAlt = Color(0xFF201C17)
+val AcentoDorado = Color(0xFFD8B36A)
+val AcentoDoradoSuave = Color(0xFFB89357)
+val TextoPrincipal = Color(0xFFEDE6DA)
+val TextoSecundario = Color(0xFFB8AFA0)
+val Error = Color(0xFFCF6679)
